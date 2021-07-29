68274aa3e26061f6dded16854ba88f5bf57118dd
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-30 00:43:46 +0300

    Update unit tests

--- R/read_ENVI_HySpex.R
+++ R/read_ENVI_HySpex.R
@@ -1,9 +1,8 @@
+# Function -------------------------------------------------------------------
 #' @describeIn  read_ENVI
 #' @include read_ENVI.R
 #' @export
-#'
-#' @concept io
-#'
+
 read_ENVI_HySpex <- function(file = stop("read_ENVI_HySpex: file name needed"),
                              headerfile = NULL, header = list(), keys.hdr2data = NULL, ...) {
   headerfile <- .find_ENVI_header(file, headerfile)
@@ -31,40 +30,26 @@ read_ENVI_HySpex <- function(file = stop("read_ENVI_HySpex: file name needed"),
   spc
 }
 
-# FIXME: fix unit tests
+# Unit tests -----------------------------------------------------------------
 
 hySpc.testthat::test(read_ENVI_HySpex) <- function() {
   context("read_ENVI_HySpex")
 
-  filename <- system.file("extdata", "HySpexNIR.hyspex", package = "hySpc.read.ENVI")
+  filename <- system.file(
+    "extdata", "HySpexNIR.hyspex",
+    package = "hySpc.read.ENVI"
+  )
+
   test_that("Hyspex ENVI file", {
-    hyspex <- hySpc.read.ENVI::read_ENVI_HySpex(filename)
+    spc_hyspex <- read_ENVI_HySpex(filename)
 
-    expect_equal(hyspex$spc[10], 114)
-    expect_equal(hyspex$spc[251], 82)
+    expect_equal(spc_hyspex$spc[10], 114)
+    expect_equal(spc_hyspex$spc[251], 82)
 
-    expect_equal(round(hyspex@wavelength[[31]], 2), 1117.64)
-    expect_equal(round(hyspex@wavelength[[119]], 2), 1594.75)
+    expect_equal(round(spc_hyspex@wavelength[[31]], 2), 1117.64)
+    expect_equal(round(spc_hyspex@wavelength[[119]], 2), 1594.75)
 
-    expect_equal(hyspex@label[[1]], "x / pixel")
-    expect_equal(hyspex@label[[3]], "I / a.u.")
-  })
-}
-
-hySpc.testthat::test(read_ENVI_HySpex) <- function() {
-  context("read_ENVI_HySpex")
-
-  filename <- system.file("extdata", "HySpexNIR.hyspex", package = "hySpc.read.ENVI")
-  test_that("Hyspex ENVI file", {
-    hyspex <- hySpc.read.ENVI::read_ENVI_HySpex(filename)
-
-    expect_equal(hyspex$spc[10], 114)
-    expect_equal(hyspex$spc[251], 82)
-
-    expect_equal(round(hyspex@wavelength[[31]], 2), 1117.64)
-    expect_equal(round(hyspex@wavelength[[119]], 2), 1594.75)
-
-    expect_equal(hyspex@label[[1]], "x / pixel")
-    expect_equal(hyspex@label[[3]], "I / a.u.")
+    expect_equal(spc_hyspex@label[[1]], "x / pixel")
+    expect_equal(spc_hyspex@label[[3]], "I / a.u.")
   })
 }
